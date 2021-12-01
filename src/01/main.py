a94cf7726987c86bd398fc83f7868381a239b999
Marc de Saint Vincent <marc.de-saint-vincent@contentsquare.com>
2021-12-01 16:14:56 +0100

    Add part 2

--- src/01/main.py
+++ src/01/main.py
@@ -1,6 +1,13 @@
 with open("input.txt") as file:
-    data = file.read().splitlines()
+    data = list(map(int, file.read().splitlines()))
 
-result = sum([int(data[index]) < int(data[index + 1]) for index in range(len(data[1:]))])
+def part1(data: list[int]) -> int:
+    result = sum([data[index] < data[index + 1] for index in range(len(data[1:]))]) 
+    return result
 
-print(result)
+def part2(data: list[int]) -> int:
+    windows = [sum(data[index:index + 3]) for index in range(len(data[1:-1]))]
+    return part1(windows)
+
+print(part1(data))
+print(part2(data))
